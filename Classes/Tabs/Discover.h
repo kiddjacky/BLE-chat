149d895eb487c90f6c136a4166cd08e6048623ae
kiddjacky <kiddjacky@kiddjackydeMacBook-Pro.local>
2015-04-17 23:40:50 -0700

    initial version

---
+++ Classes/Tabs/Discover.h
@@ -0,0 +1,13 @@
+//
+//  Discover.h
+//  app
+//
+//  Created by kiddjacky on 3/22/15.
+//  Copyright (c) 2015 KZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Discover : UITableView
+
+@end
---
+++ Classes/Tabs/DiscoversView.h
@@ -0,0 +1,13 @@
+//
+//  DiscoversView.h
+//  app
+//
+//  Created by kiddjacky on 3/22/15.
+//  Copyright (c) 2015 KZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiscoversView : UITableViewController
+
+@end
