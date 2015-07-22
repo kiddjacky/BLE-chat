f312eb4d0ac00ede04d3da42a00644079d3f4f0f
dandanlau <kobe.daniel@gmail.com>
2015-07-21 23:12:50 -0700

    changed cell width

---
+++ Classes/Tabs/contactsCell.h
@@ -0,0 +1,13 @@
+//
+//  contactsCell.h
+//  app
+//
+//  Created by Daniel Lau on 7/20/15.
+//  Copyright (c) 2015 KZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface contactsCell : UITableViewCell
+
+@end
---
+++ Classes/Tabs/contactsCell.m
@@ -0,0 +1,23 @@
+//
+//  contactsCell.m
+//  app
+//
+//  Created by Daniel Lau on 7/20/15.
+//  Copyright (c) 2015 KZ. All rights reserved.
+//
+
+#import "contactsCell.h"
+
+@implementation contactsCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
